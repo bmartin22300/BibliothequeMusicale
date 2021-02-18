4734943fdd16a676f79de6ddfc01805e3ca77f8c
Baptiste <bmartin@enssat.fr>
2021-02-18 22:37:44 +0100

    Init JEE Project's architecture

---
+++ src/Servlet/MainServlet.java
@@ -0,0 +1,48 @@
+package Servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MainServlet extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+			doProcess(request,response);
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+			doProcess(request,response);
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void doProcess(HttpServletRequest request, HttpServletResponse response) throws NumberFormatException, SQLException {
+        String pageName="/Accueil.jsp";
+        RequestDispatcher rd = getServletContext().getRequestDispatcher(pageName);
+        try {
+              rd.forward(request, response);
+        } catch (ServletException e) {
+              e.printStackTrace();
+        } catch (IOException e) {
+              e.printStackTrace();
+        }
+  }
+
+}
